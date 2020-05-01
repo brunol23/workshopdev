6aad54bff4eb0e63c9ebe8407d92d6fe5c8d5d99
BrunoLuna23 <54488051+BrunoLuna23@users.noreply.github.com>
2020-05-01 00:10:48 -0300

    Primeiro commit

---
+++ public/scripts.js
@@ -0,0 +1,44 @@
+function onOff(){
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+
+    document
+        .querySelector("body")
+        .classList
+        .toogle("hideScroll")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toogle("addScroll")
+}
+
+function checkFields(event) {
+
+    const valuesToCheck = [
+        "title",
+        "category",
+        "image",
+        "description",
+        "link",
+    ]
+
+    const isEmpty = valuesToCheck.find(function(value) {
+
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIfIsEmpty = !event.target[value].value.trim()
+        
+        if(checkIfIsString  && checkIfIsEmpty) {
+            return true
+        }
+
+    })
+
+    if(isEmpty) {
+        event.preventDefault()
+        alert("Por Favor, preencha todos os campos!!!")
+    }
+
+}
